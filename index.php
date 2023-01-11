f9e5b294b5067f542d2173aab641ff7ee4209dc3
Nekruz68 <117161966+Nekruz68@users.noreply.github.com>
2023-01-11 13:55:05 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<title> My page </title>
+<link rel="stylesheet" href="mycss.css"
+<link rel="shortcut icon" href="favicon.iso">
+</head>
+<body>
+<div class="mydiv1">
+<?php
+echo "Квадрат разности чисел 6 и 3 равен ";
+echo 6*6-2*6*3+3*3
+?>
+</div>
+</body>
+</html>
+
+
+
